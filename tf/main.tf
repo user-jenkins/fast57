7ede81260f95820a3122b47e349fe440865ab042
user-jenkins <79544295+user-jenkins@users.noreply.github.com>
2025-07-19 15:19:23 -0400

    Add/update tf/main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_eks_cluster" "fast57" {
-  name     = "fast57-eks-cluster"
-  role_arn = aws_iam_role.fast57-eks.arn
+  name     = "fast57-eks"
+  role_arn = aws_iam_role.fast57.arn
 }
