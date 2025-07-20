3e08a811ae4a5f8b1519e49be05ce72901121ab6
user-jenkins <79544295+user-jenkins@users.noreply.github.com>
2025-07-20 16:32:02 -0400

    Add/update tf/main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_eks_cluster" "fast57-eks" {
-  name     = "fast57-eks"
+  name     = "fast57-eks-cluster"
   role_arn = aws_iam_role.fast57-eks.arn
+  depends_on = [
+    aws_iam_role_policy_attachment.fast57-eks-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.fast57-eks-AmazonEKSVPCResourceController
+  ]
 }
