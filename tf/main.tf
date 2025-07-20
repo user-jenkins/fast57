83e88d621d757ea4aec127eaabbda1acf74dd61f
user-jenkins <79544295+user-jenkins@users.noreply.github.com>
2025-07-20 00:49:23 -0400

    Add/update tf/main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -5,19 +5,3 @@ resource "aws_eks_cluster" "fast57-eks" {
   name     = "fast57-eks"
   role_arn = aws_iam_role.fast57-eks.arn
 }
-resource "aws_iam_role" "fast57-eks" {
-  name        = "fast57-eks"
-  description = "EKS Cluster Role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
